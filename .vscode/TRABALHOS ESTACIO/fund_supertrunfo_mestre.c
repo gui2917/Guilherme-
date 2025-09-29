dda807e2a110d4ccab6060d68f78f2e442669f9a
gui2917 <155523280+gui2917@users.noreply.github.com>
2025-09-29 14:41:09 +0000

    FALTA FINALIZAR NÍVEL fund_supertrunfo_mestre

---
+++ .vscode/TRABALHOS ESTACIO/fund_supertrunfo_mestre.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+
+int main (){
+
+    char estado1;
+    char codigo1[4];
+    char cidade1[50];
+    unsigned long int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+    float densidadePopulacional1;
+    float pibperCapita1;
+    float superPoder1;
+
+    char estado2;
+    char codigo2[4];
+    char cidade2[50];
+    unsigned long int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
+    float densidadePopulacional2;
+    float pibperCapita2;
+    float superPoder2;
+
+    printf("\nCRIANDO CARTAS DESAFIO SUPER TRUNFO - NÍVEL NOVATO\n");
+    printf("------------------------------------------------------------\n");
+    printf("======Carta 1=======\n");
+
+    printf("\nDigite o Estado (A-H):");
+    scanf(" %c", &estado1);
+    // espaço antes do "%c" evita pegar lixo do ENTER;
+
+    printf("Digite o Código da Carta:\n");
+    scanf("%s", codigo1);
+    //Obs: Em especificador de formato de String "%s", não usa o &, pois é um Array.
+
+    printf("Digite o nome do Estado:\n");
+    scanf("%s", cidade1);
+
+    printf("Digite a População:\n");
+    scanf("%lu", &populacao1);
+
+    printf("Digite a Área em Km²:\n");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB em Bilhões de Reais:\n");
+    scanf("%f", &pib1);
+
+    printf("Digite a quantidade de Pontos Turísticos:\n");
+    scanf("%d", &pontosTuristicos1);
+
+    printf("Cálculo da Carta 1\n");
+
+    densidadePopulacional1 = populacao1 / area1;
+    pibperCapita1 = (pib1 * 1000000000.0) / populacao1;
+    superPoder1 = (float)populacao1 + area1 + pib1 + pontosTuristicos1 + pibperCapita1 + 
+    (1.0 / densidadePopulacional1);
+
+
+    printf("======Carta 2=======\n");
+
+    printf("\nDigite o Estado (A-H):");
+    scanf(" %c", &estado2);
+    
+    printf("Digite o Código da Carta:\n");
+    scanf("%s", codigo2);
+
+    printf("Digite o nome do Estado:\n");
+    scanf("%s", cidade2);
+
+    printf("Digite a População:\n");
+    scanf("%lu", &populacao2);
+
+    printf("Digite a Área em Km²:\n");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB em Bilhões de Reais:\n");
+    scanf("%f", &pib2);
+
+    printf("Digite a quantidade de Pontos Turísticos:\n");
+    scanf("%d", &pontosTuristicos2);
+
+    printf("Cálculo da Carta 2\n");
+
+    densidadePopulacional2 = populacao2 / area2;
+    pibperCapita2 = (pib2 * 1000000000.0) / populacao2;
+    superPoder2 = (float)populacao2 + area2 + pib2 + pontosTuristicos2 + pibperCapita2 + 
+    (1.0 / densidadePopulacional2);
+
+
+    printf("EXIBIÇÃO DOS DADOS\n");
+
+    printf("\n======= CARTA 1 ======\n");
+
+    printf("\nEstado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", cidade1);
+    printf("População: %lu\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f\n", densidadePopulacional1);
+    printf("Pib Per Capita: %.2f\n", pibperCapita1);
+
+     printf("\n======= CARTA 2 ======\n");
+
+   
+    printf("\nEstado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", cidade2);
+    printf("População: %lu\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional: %.2f\n", densidadePopulacional2);
+    printf("Pib Per Capita: %.2f\n", pibperCapita2);
+
+    printf("======COMPARAÇÃO DAS CARTAS======\n");
+
+
+
+
+
+    return 0;
+}
